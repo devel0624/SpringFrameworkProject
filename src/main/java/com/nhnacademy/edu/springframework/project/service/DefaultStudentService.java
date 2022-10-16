413471277b070f768c187076dc3ffe178b7bba94
SEONG JUN LEE <67360314+devel0624@users.noreply.github.com>
2022-10-16 20:58:32 +0900

    TODO 4 edit DefaultStudentService.getStudentsOrderByScore add stream.sorted

--- src/main/java/com/nhnacademy/edu/springframework/project/service/DefaultStudentService.java
+++ src/main/java/com/nhnacademy/edu/springframework/project/service/DefaultStudentService.java
@@ -6,6 +6,7 @@ import com.nhnacademy.edu.springframework.project.repository.Students;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 
 public class DefaultStudentService implements StudentService {
@@ -23,7 +24,7 @@ public class DefaultStudentService implements StudentService {
         Students studentRepository = CsvStudents.getInstance();
         // TODO 4 : 성적 순으로 학생 정보(Student)를 반환합니다.
         // 소팅 문제입니다. Java Stream API 의 소팅 관련 메서드를 사용하세요.
-        return studentRepository.findAll();
+        return studentRepository.findAll().stream().sorted(Comparator.comparingInt(student -> student.getScore().getScore())).collect(Collectors.toCollection(ArrayList::new));
     }
 
 }
